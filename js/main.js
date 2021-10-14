3b37b772e21f30ac19c5796977c7f65029896def
Malza0408 <parkjh04080@gmail.com>
2021-10-15 01:14:10 +0900

    bacground img

--- js/main.js
+++ js/main.js
@@ -17,6 +17,7 @@ const CARROT = "carrot";
 const BUG = "bug";
 const imgsArr = [];
 
+let id;
 function uuidv4() {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
     var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
@@ -24,14 +25,13 @@ function uuidv4() {
   });
 }
 
+// init
 for (let i = 0; i < 10; i++) {
   const carrot = makeElements(CARROT);
   field.appendChild(carrot);
   const bug = makeElements(BUG);
   field.appendChild(bug);
 }
-
-let id;
 
 function makeRandomPisition() {
   let X = Math.floor(Math.random() * (fieldRect.right - fieldRect.x));
